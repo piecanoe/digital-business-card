272c9d9cccf170b8b20a1dbdbdd2936f03516f6a
Captain Du <karenkaydu@gmail.com>
2023-12-19 12:28:30 -0800

    Fix Font Awesome icons in Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     return (
         <div className="socials">
-            <a href="https://www.linkedin.com/in/karenkdu/"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true" href="https://www.linkedin.com/in/karenkdu/"></i></a>
-            <a href="https://twitter.com/karendufus"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/in/karenkdu/">
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+            </a>
+            <a href="https://twitter.com/karendufus">
+            <FontAwesomeIcon icon={faSquareTwitter} size="2xl" />
+            </a>
         </div>
     )
 }
